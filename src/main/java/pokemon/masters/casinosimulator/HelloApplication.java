4cad4c60bb89d75a5371935a8c3667ddfb22b822
choffman4 <choffman@student.neumont.edu>
2023-02-07 10:10:03 -0700

    Changed Default size of window when launching program, changed title of window to Casino Simulator Menu

--- src/main/java/pokemon/masters/casinosimulator/HelloApplication.java
+++ src/main/java/pokemon/masters/casinosimulator/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1050, 650);
+        stage.setTitle("Casino Simulator Menu");
         stage.setScene(scene);
         stage.show();
     }
