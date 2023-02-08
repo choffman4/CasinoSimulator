242b1c475a6ffb8b01c21751a9fdbc377ed27fe7
choffman4 <choffman@student.neumont.edu>
2023-02-08 10:40:31 -0700

    added changeScene class which is a class with a function that allows changing scences/pages. Function takes a name of the fxml page. concatenates the path onto the front. Then loades the scene

--- src/main/java/pokemon/masters/casinosimulator/HelloApplication.java
+++ src/main/java/pokemon/masters/casinosimulator/HelloApplication.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //First window to open when program is launched is "chooseGameView.fxml"
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chooseGameView.fxml"));
+        //Default Window size for application (1050 W, 650 H)
         Scene scene = new Scene(fxmlLoader.load(), 1050, 650);
         stage.setTitle("Casino Simulator Menu");
         stage.setScene(scene);
