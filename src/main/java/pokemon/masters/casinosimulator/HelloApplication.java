2ceb6b269cb3d9194d9162bb6a356020da9d5735
choffman4 <choffman@student.neumont.edu>
2023-02-08 10:32:33 -0700

    removal of hello controller, hello-viewfxml, Updated Application to open chooseGameView.fxml

--- src/main/java/pokemon/masters/casinosimulator/HelloApplication.java
+++ src/main/java/pokemon/masters/casinosimulator/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("chooseGameView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1050, 650);
         stage.setTitle("Casino Simulator Menu");
         stage.setScene(scene);
