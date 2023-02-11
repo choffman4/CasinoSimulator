e64470b0a98b0b88e37a310feb98316a40417359
choffman4 <choffman@student.neumont.edu>
2023-02-10 19:21:51 -0700

    roulette chips can be selected and arrows shown over chip selected

--- src/main/java/pokemon/masters/casinosimulator/controllers/RouletteController.java
+++ src/main/java/pokemon/masters/casinosimulator/controllers/RouletteController.java
@@ -14,8 +14,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import pokemon.masters.casinosimulator.gamelogic.Player;
 import pokemon.masters.casinosimulator.gamelogic.Roulette;
 import pokemon.masters.casinosimulator.services.ChangeScene;
+import pokemon.masters.casinosimulator.services.FormatMoney;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -27,15 +29,27 @@ public class RouletteController {
 
     private final Image[] chipImage = new Image[36];
 
-    Roulette game = new Roulette();
+    private final Roulette game = new Roulette();
 
     @FXML
-    GridPane gridNumbers;
+    private GridPane gridNumbers;
     @FXML
-    ImageView img0, img19to36, img1st12, img1to18, img2nd12, img2to1bottom, img2to1middle,
-            img2to1top, img3rd12, imgArrowMarker1, imgArrowMarker10, imgArrowMarker20, imgArrowMarker30,
-            imgArrowMarker5, imgArrowMarker6, imgArrowMarker7, imgArrowMarker8, imgBlackDiamond, imgEVEN, imgODD,
+    private ImageView img19to36, img1st12, img1to18, img2nd12, img2to1bottom, img2to1middle,
+            img2to1top, img3rd12, imgArrowMarker1, imgArrowMarker10, imgArrowMarker20, imgArrowMarker50, imgArrowMarker100,
+            imgArrowMarker5, imgArrowMarker500, imgArrowMarker1k, imgArrowMarker5k, imgBlackDiamond, imgEVEN, imgODD,
             imgRedDiamond, imgBack, imgRules, imgSpin, imgWheel;
+
+
+    @FXML
+    private StackPane btn0, btn1,  btn2,  btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16,
+    btn17, btn18, btn19, btn20, btn21, btn22, btn23, btn24, btn25, btn26, btn27, btn28, btn29, btn30, btn31, btn32,
+    btn33, btn34, btn35, btn36;
+
+    @FXML
+    private ImageView img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16,
+    img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32,
+    img33, img34, img35, img36;
+
     @FXML
     private StackPane btnBack, btnRules, btnSpin;
     @FXML
@@ -56,8 +70,24 @@ public class RouletteController {
         //Initialize buttons in grid number 1-36
         initializeBettingButtons();
 
+        txtChipsAmount.setText(FormatMoney.Format(Player.getChipMoney()));
+
         circleResultColor.setVisible(false);
         txtResultNumber.setVisible(false);
+
+        resetArrows();
+    }
+
+    private void resetArrows() {
+        imgArrowMarker1.setVisible(false);
+        imgArrowMarker5.setVisible(false);
+        imgArrowMarker10.setVisible(false);
+        imgArrowMarker20.setVisible(false);
+        imgArrowMarker50.setVisible(false);
+        imgArrowMarker100.setVisible(false);
+        imgArrowMarker500.setVisible(false);
+        imgArrowMarker1k.setVisible(false);
+        imgArrowMarker5k.setVisible(false);
     }
 
     //loop through grid and add a stack pane with child image
@@ -81,80 +111,80 @@ public class RouletteController {
 
     //These are all the buttons and images associated with each of the numbers 1-36
     private void initializeBettingButtons() {
-        StackPane btn1 = (StackPane) gridNumbers.getChildren().get(0);
-        ImageView img1 = (ImageView) btn1.getChildren().get(0);
-        StackPane btn2 = (StackPane) gridNumbers.getChildren().get(1);
-        ImageView img2 = (ImageView) btn2.getChildren().get(0);
-        StackPane btn3 = (StackPane) gridNumbers.getChildren().get(2);
-        ImageView img3 = (ImageView) btn3.getChildren().get(0);
-        StackPane btn4 = (StackPane) gridNumbers.getChildren().get(3);
-        ImageView img4 = (ImageView) btn4.getChildren().get(0);
-        StackPane btn5 = (StackPane) gridNumbers.getChildren().get(4);
-        ImageView img5 = (ImageView) btn5.getChildren().get(0);
-        StackPane btn6 = (StackPane) gridNumbers.getChildren().get(5);
-        ImageView img6 = (ImageView) btn6.getChildren().get(0);
-        StackPane btn7 = (StackPane) gridNumbers.getChildren().get(6);
-        ImageView img7 = (ImageView) btn7.getChildren().get(0);
-        StackPane btn8 = (StackPane) gridNumbers.getChildren().get(7);
-        ImageView img8 = (ImageView) btn8.getChildren().get(0);
-        StackPane btn9 = (StackPane) gridNumbers.getChildren().get(8);
-        ImageView img9 = (ImageView) btn9.getChildren().get(0);
-        StackPane btn10 = (StackPane) gridNumbers.getChildren().get(9);
-        ImageView img10 = (ImageView) btn10.getChildren().get(0);
-        StackPane btn11 = (StackPane) gridNumbers.getChildren().get(10);
-        ImageView img11 = (ImageView) btn11.getChildren().get(0);
-        StackPane btn12 = (StackPane) gridNumbers.getChildren().get(11);
-        ImageView img12 = (ImageView) btn12.getChildren().get(0);
+        btn1 = (StackPane) gridNumbers.getChildren().get(0);
+        img1 = (ImageView) btn1.getChildren().get(0);
+        btn2 = (StackPane) gridNumbers.getChildren().get(1);
+        img2 = (ImageView) btn2.getChildren().get(0);
+        btn3 = (StackPane) gridNumbers.getChildren().get(2);
+        img3 = (ImageView) btn3.getChildren().get(0);
+        btn4 = (StackPane) gridNumbers.getChildren().get(3);
+        img4 = (ImageView) btn4.getChildren().get(0);
+        btn5 = (StackPane) gridNumbers.getChildren().get(4);
+        img5 = (ImageView) btn5.getChildren().get(0);
+        btn6 = (StackPane) gridNumbers.getChildren().get(5);
+        img6 = (ImageView) btn6.getChildren().get(0);
+        btn7 = (StackPane) gridNumbers.getChildren().get(6);
+        img7 = (ImageView) btn7.getChildren().get(0);
+        btn8 = (StackPane) gridNumbers.getChildren().get(7);
+        img8 = (ImageView) btn8.getChildren().get(0);
+        btn9 = (StackPane) gridNumbers.getChildren().get(8);
+        img9 = (ImageView) btn9.getChildren().get(0);
+        btn10 = (StackPane) gridNumbers.getChildren().get(9);
+        img10 = (ImageView) btn10.getChildren().get(0);
+        btn11 = (StackPane) gridNumbers.getChildren().get(10);
+        img11 = (ImageView) btn11.getChildren().get(0);
+        btn12 = (StackPane) gridNumbers.getChildren().get(11);
+        img12 = (ImageView) btn12.getChildren().get(0);
 
-        StackPane btn13 = (StackPane) gridNumbers.getChildren().get(12);
-        ImageView img13 = (ImageView) btn13.getChildren().get(0);
-        StackPane btn14 = (StackPane) gridNumbers.getChildren().get(13);
-        ImageView img14 = (ImageView) btn14.getChildren().get(0);
-        StackPane btn15 = (StackPane) gridNumbers.getChildren().get(14);
-        ImageView img15 = (ImageView) btn15.getChildren().get(0);
-        StackPane btn16 = (StackPane) gridNumbers.getChildren().get(15);
-        ImageView img16 = (ImageView) btn16.getChildren().get(0);
-        StackPane btn17 = (StackPane) gridNumbers.getChildren().get(16);
-        ImageView img17 = (ImageView) btn17.getChildren().get(0);
-        StackPane btn18 = (StackPane) gridNumbers.getChildren().get(17);
-        ImageView img18 = (ImageView) btn18.getChildren().get(0);
-        StackPane btn19 = (StackPane) gridNumbers.getChildren().get(18);
-        ImageView img19 = (ImageView) btn19.getChildren().get(0);
-        StackPane btn20 = (StackPane) gridNumbers.getChildren().get(19);
-        ImageView img20 = (ImageView) btn20.getChildren().get(0);
-        StackPane btn21 = (StackPane) gridNumbers.getChildren().get(20);
-        ImageView img21 = (ImageView) btn21.getChildren().get(0);
-        StackPane btn22 = (StackPane) gridNumbers.getChildren().get(21);
-        ImageView img22 = (ImageView) btn22.getChildren().get(0);
-        StackPane btn23 = (StackPane) gridNumbers.getChildren().get(22);
-        ImageView img23 = (ImageView) btn23.getChildren().get(0);
-        StackPane btn24 = (StackPane) gridNumbers.getChildren().get(23);
-        ImageView img24 = (ImageView) btn24.getChildren().get(0);
+        btn13 = (StackPane) gridNumbers.getChildren().get(12);
+        img13 = (ImageView) btn13.getChildren().get(0);
+        btn14 = (StackPane) gridNumbers.getChildren().get(13);
+        img14 = (ImageView) btn14.getChildren().get(0);
+        btn15 = (StackPane) gridNumbers.getChildren().get(14);
+        img15 = (ImageView) btn15.getChildren().get(0);
+        btn16 = (StackPane) gridNumbers.getChildren().get(15);
+        img16 = (ImageView) btn16.getChildren().get(0);
+        btn17 = (StackPane) gridNumbers.getChildren().get(16);
+        img17 = (ImageView) btn17.getChildren().get(0);
+        btn18 = (StackPane) gridNumbers.getChildren().get(17);
+        img18 = (ImageView) btn18.getChildren().get(0);
+        btn19 = (StackPane) gridNumbers.getChildren().get(18);
+        img19 = (ImageView) btn19.getChildren().get(0);
+        btn20 = (StackPane) gridNumbers.getChildren().get(19);
+        img20 = (ImageView) btn20.getChildren().get(0);
+        btn21 = (StackPane) gridNumbers.getChildren().get(20);
+        img21 = (ImageView) btn21.getChildren().get(0);
+        btn22 = (StackPane) gridNumbers.getChildren().get(21);
+        img22 = (ImageView) btn22.getChildren().get(0);
+        btn23 = (StackPane) gridNumbers.getChildren().get(22);
+        img23 = (ImageView) btn23.getChildren().get(0);
+        btn24 = (StackPane) gridNumbers.getChildren().get(23);
+        img24 = (ImageView) btn24.getChildren().get(0);
 
-        StackPane btn25 = (StackPane) gridNumbers.getChildren().get(24);
-        ImageView img25 = (ImageView) btn25.getChildren().get(0);
-        StackPane btn26 = (StackPane) gridNumbers.getChildren().get(25);
-        ImageView img26 = (ImageView) btn26.getChildren().get(0);
-        StackPane btn27 = (StackPane) gridNumbers.getChildren().get(26);
-        ImageView img27 = (ImageView) btn27.getChildren().get(0);
-        StackPane btn28 = (StackPane) gridNumbers.getChildren().get(27);
-        ImageView img28 = (ImageView) btn28.getChildren().get(0);
-        StackPane btn29 = (StackPane) gridNumbers.getChildren().get(28);
-        ImageView img29 = (ImageView) btn29.getChildren().get(0);
-        StackPane btn30 = (StackPane) gridNumbers.getChildren().get(29);
-        ImageView img30 = (ImageView) btn30.getChildren().get(0);
-        StackPane btn31 = (StackPane) gridNumbers.getChildren().get(30);
-        ImageView img31 = (ImageView) btn31.getChildren().get(0);
-        StackPane btn32 = (StackPane) gridNumbers.getChildren().get(31);
-        ImageView img32 = (ImageView) btn32.getChildren().get(0);
-        StackPane btn33 = (StackPane) gridNumbers.getChildren().get(32);
-        ImageView img33 = (ImageView) btn33.getChildren().get(0);
-        StackPane btn34 = (StackPane) gridNumbers.getChildren().get(33);
-        ImageView img34 = (ImageView) btn34.getChildren().get(0);
-        StackPane btn35 = (StackPane) gridNumbers.getChildren().get(34);
-        ImageView img35 = (ImageView) btn35.getChildren().get(0);
-        StackPane btn36 = (StackPane) gridNumbers.getChildren().get(35);
-        ImageView img36 = (ImageView) btn36.getChildren().get(0);
+        btn25 = (StackPane) gridNumbers.getChildren().get(24);
+        img25 = (ImageView) btn25.getChildren().get(0);
+        btn26 = (StackPane) gridNumbers.getChildren().get(25);
+        img26 = (ImageView) btn26.getChildren().get(0);
+        btn27 = (StackPane) gridNumbers.getChildren().get(26);
+        img27 = (ImageView) btn27.getChildren().get(0);
+        btn28 = (StackPane) gridNumbers.getChildren().get(27);
+        img28 = (ImageView) btn28.getChildren().get(0);
+        btn29 = (StackPane) gridNumbers.getChildren().get(28);
+        img29 = (ImageView) btn29.getChildren().get(0);
+        btn30 = (StackPane) gridNumbers.getChildren().get(29);
+        img30 = (ImageView) btn30.getChildren().get(0);
+        btn31 = (StackPane) gridNumbers.getChildren().get(30);
+        img31 = (ImageView) btn31.getChildren().get(0);
+        btn32 = (StackPane) gridNumbers.getChildren().get(31);
+        img32 = (ImageView) btn32.getChildren().get(0);
+        btn33 = (StackPane) gridNumbers.getChildren().get(32);
+        img33 = (ImageView) btn33.getChildren().get(0);
+        btn34 = (StackPane) gridNumbers.getChildren().get(33);
+        img34 = (ImageView) btn34.getChildren().get(0);
+        btn35 = (StackPane) gridNumbers.getChildren().get(34);
+        img35 = (ImageView) btn35.getChildren().get(0);
+        btn36 = (StackPane) gridNumbers.getChildren().get(35);
+        img36 = (ImageView) btn36.getChildren().get(0);
     }
 
     //Get's game images from resources
@@ -192,52 +222,136 @@ public class RouletteController {
     //These buttons are used for changing the bet amount
     @FXML
     void betFifty(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker50.setVisible(true);
+        game.setCurrentChipBets(50);
     }
 
     @FXML
     void betFive(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker5.setVisible(true);
+        game.setCurrentChipBets(5);
     }
 
     @FXML
     void betFiveHundred(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker500.setVisible(true);
+        game.setCurrentChipBets(500);
     }
 
     @FXML
     void betFiveThousand(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker5k.setVisible(true);
+        game.setCurrentChipBets(5000);
     }
 
     @FXML
     void betOne(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker1.setVisible(true);
+        game.setCurrentChipBets(1);
     }
 
     @FXML
     void betOneHundred(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker100.setVisible(true);
+        game.setCurrentChipBets(100);
     }
 
     @FXML
     void betOneThousand(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker1k.setVisible(true);
+        game.setCurrentChipBets(1000);
     }
 
     @FXML
     void betTen(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker10.setVisible(true);
+        game.setCurrentChipBets(10);
     }
 
     @FXML
     void betTwenty(MouseEvent event) {
-
+        resetArrows();
+        imgArrowMarker20.setVisible(true);
+        game.setCurrentChipBets(20);
     }
 
     //This function opens a rule window
     @FXML
     void onRules(MouseEvent event) {
+
+    }
+
+
+    @FXML
+    void onZero(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onBlack(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onEven(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onOdd(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onRed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on19to36(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on1st12(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on1to18(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on2nd12(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on2to1bot(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on2to1mid(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on2to1top(MouseEvent event) {
+
+    }
+
+    @FXML
+    void on3rd12(MouseEvent event) {
 
     }
 
--- src/main/java/pokemon/masters/casinosimulator/gamelogic/Roulette.java
+++ src/main/java/pokemon/masters/casinosimulator/gamelogic/Roulette.java
@@ -5,10 +5,27 @@ import java.util.Random;
 public class Roulette {
     private int roleColor;
     Random random = new Random();
+    private int currentBetPos;
+    private int currentChipBets;
+
+    public int getCurrentBetPos() {
+        return currentBetPos;
+    }
+
+    public void setCurrentBetPos(int currentBetPos) {
+        this.currentBetPos = currentBetPos;
+    }
+
+    public int getCurrentChipBets() {
+        return currentChipBets;
+    }
+
+    public void setCurrentChipBets(int currentChipBets) {
+        this.currentChipBets = currentChipBets;
+    }
 
     public int getRoleNumber() {
-        int roleNumber = random.nextInt(37);
-        return roleNumber;
+        return random.nextInt(37);
     }
 
     public int getRoleColor() {
